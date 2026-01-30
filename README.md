862fc4fa6d87ce0f854d57d2ffbf13fffa69dd6e
June <lycssa@gmail.com>
2026-01-29 21:32:21 -0800

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Neovim
+# Minimalist Neovim Configuration
+
+This config only installs six plugins by default. These plugins are managed by
+lazy.nvim, a popular plugin manager. Run `:Lazy` to access lazy.nvim's menu.
+Also, make sure these plugins' prerequisites have been installed, such as
+`tree-sitter-cli` for Nvim-Treesitter.
+
+1. Mason (Installs LSPs, Formatters, and more!)
+2. Nvim-Treesitter (Syntax Highlighting)
+3. Conform (Uses Mason's formatters to format on save)
+4. Blink.cmp (Uses Mason's LSPs to provide autocomplete)
+4. Nvim-Autopairs (Autoclose brackets, parentheses, etc.)
+5. Catppuccin (Dark theme for Neovim)
+
+When installing languages, the first four plugins must be configured in order
+to support it.
+
+1. Mason
+    - Run `:Mason` to open the menu.
+    - <C-f> to filter by a language.
+    - Hover and press `i` to install the packages you want.
+2. Nvim-Treesitter
+    - Run `:TSInstall <language>` to install treesitter's parser for that
+    language.
+    - Inside init.lua, at the autocmd for `vim.treesitter.start()`, list the
+    language the parser was just installed for.
+3. Conform
+    - To connect conform to the formatter installed by Mason, add
+    `<language> = "formatter"` at the `formatters_by_ft` option.
+    - Example: `java = "google-java-format"`
+4. LSP Configuration
+    - Write the configuration for your language server.
+    - Example:
+
+```lua
+return {
+    cmd = { "jdtls" },
+    filetypes = { "java" },
+    root_marker = { "pom.xml", "build.gradle", ".git", "mvnw", "gradlew" },
+}
+```
+
+5. Blink and LSP Enabling
+    - Option 1: Create a `lsp` folder inside the `.config/nvim/` folder.
+    Inside `lsp`, create a `.lua` file returning the configuration.
+    - Option 2: Use `vim.lsp.config()` and put your configuration inside.
+    This happens at the bottom of `init.lua`.
+    - After either option 1 or 2, go to the bottom of `init.lua` and add
+    `vim.lsp.enable()` for the installed language server.
+    - This step allows blink.cmp to work as well.
+
+Use `:checkhealth` to check that all plugins are working properly.
+
---
+++ init.lua
@@ -0,0 +1,93 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.termguicolors = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.tabstop = 4
+
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = {
+		-- add your plugins here
+		{
+			"catppuccin/nvim",
+			name = "catppuccin",
+			opts = {
+				color_overrides = {
+					mocha = {
+						text = "#ffffff",
+						base = "#000000",
+						mantle = "#000000",
+						crust = "#1e1e2e",
+					},
+				},
+				no_italic = true,
+				term_colors = true,
+			},
+			priority = 1000,
+		},
+		{ "mason-org/mason.nvim", opts = {} },
+		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", lazy = false },
+		{
+			"saghen/blink.cmp",
+			dependencies = { "rafamadriz/friendly-snippets" },
+			opts = {},
+			opts_extend = { "sources.default" },
+			version = "1.*",
+		},
+		{
+			"stevearc/conform.nvim",
+			opts = {
+				format_on_save = { lsp_format = "fallback", timeout_ms = 500 },
+				formatters_by_ft = {}, -- List formatters here
+			},
+		},
+		{
+			"windwp/nvim-autopairs",
+			event = "InsertEnter",
+			config = true,
+		},
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "catppuccin" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
+
+-- Setup other plugins
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "<filetype>" }, -- List filetypes here
+	callback = function()
+		vim.treesitter.start()
+	end,
+})
+vim.cmd.colorscheme("catppuccin")
+vim.diagnostic.config({
+	virtual_text = true,
+})
+-- vim.lsp.enable() after it's configured
