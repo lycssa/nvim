980887443e1c07b485b7221fc8988856e4f81119
June <lycssa@gmail.com>
2026-04-22 23:29:27 -0700

    Enable treesitter's indent to fix indentation in python

--- init.lua
+++ init.lua
@@ -99,6 +99,7 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "<filetype>" }, -- List filetypes here
 	callback = function()
 		vim.treesitter.start()
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 	end,
 })
 vim.cmd.colorscheme("catppuccin")
